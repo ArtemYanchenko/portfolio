565db777c7d54f01f7c92134c3ce2b62581d96e6
Artem Yanchenko <artem.yanchenko.94@mail.ru>
2023-06-12 23:45:42 +0300

    add ToggleDarkMode

--- src/nav/Nav.tsx
+++ src/nav/Nav.tsx
@@ -5,6 +5,7 @@ const Nav = () => {
     return (
         <div className={classes.nav}>
             <a href="" className={classes.navLink}>Main</a>
+            <a href="" className={classes.navLink}>About</a>
             <a href="" className={classes.navLink}>Skills</a>
             <a href="" className={classes.navLink}>Projects</a>
             <a href="" className={classes.navLink}>Contact Me</a>
